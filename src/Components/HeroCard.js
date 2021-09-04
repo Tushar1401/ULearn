99369d9ccde79c82670ee53761304e53e912a98a
Tushar1401 <tushararora1401@gmail.com>
2021-09-04 20:55:13 +0530

    minor changes in herocard

--- src/Components/HeroCard.js
+++ src/Components/HeroCard.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import './HeroCard.css'
 import photo from '../Images/hero-card-img.svg'
+import { Link } from 'react-router-dom'
 
 function HeroCard() {
     return (
         <div className="hero-card-wrapper">
             <div className="left">
                 <div className="heading">
-                    <p>Start Your</p> 
+                    <p>Start Your</p>
                     <p id="heading-hero">Learning <span>Now !</span></p>
                 </div>
                 <div className="discription">
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod odio non leo pretium pellentesque. Curabitur blandit urna cursus, malesuada erat ut, egestas odio. Quisque suscipit, urna ac vulputate sollicitudin, mi urna elementum
+                    ULearn is a one-stop free solution for all the users who want to learn something new & gain knowledge without losing focus. It also provides a unique platform to all the content creators, teachers, writers and others who want to give back to the society by creating quality content for those who are keen to learn some new skills. The platform will enable them to create content through video, audio, power point presentation or texts along with real life examples. 
+
                     </p>
                 </div>
                 <div className="cta">
-                    <button className="hero-card-btn">
-                        Become an Educator
-                    </button>
+                    <Link to='/login' style={{ textDecoration: 'none' }}>
+                        <button className="hero-card-btn">
+                            Become an Educator
+                        </button>
+                    </Link>
                 </div>
             </div>
             <div className="right">
